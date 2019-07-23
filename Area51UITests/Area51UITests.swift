a33b54b3afc32913b099853ad39d73b867653e29
rolando <kinglouis.belmont@gmail.com>
2019-07-23 16:45:50 -0700

    added a first very simple test

--- Area51UITests/Area51UITests.swift
+++ Area51UITests/Area51UITests.swift
@@ -15,19 +15,16 @@ class Area51UITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        // Must launch the application. This makes sure it happens for each test method.
         XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testPopularHeader() {
+      let app = XCUIApplication()
+      XCTAssert(app.navigationBars["Popular"].otherElements["Popular"].exists)
     }
-
 }
---
+++ Area51UITests/MainScreen.swift
@@ -0,0 +1,30 @@
+//
+//  MainScreen.swift
+//  Area51UITests
+//
+//  Created by Rolando Navarrete on 7/23/19.
+//
+
+import XCTest
+
+class MainScreen {
+    struct Subreddits {
+        let popular: XCUIElement
+        let news: XCUIElement
+        let gadgets: XCUIElement
+        let spots: XCUIElement
+        let gaming: XCUIElement
+        let pics: XCUIElement
+        let worldnews: XCUIElement
+    }
+
+//    static let subreddits = Subreddits(
+//        popular: <#T##XCUIElement#>,
+//        news: <#T##XCUIElement#>,
+//        gadgets: <#T##XCUIElement#>,
+//        spots: <#T##XCUIElement#>,
+//        gaming: <#T##XCUIElement#>,
+//        pics: <#T##XCUIElement#>,
+//        worldnews: <#T##XCUIElement#>
+//    )
+}
